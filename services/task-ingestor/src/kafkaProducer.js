f662aabfef9c1d99f57c593db1d23286ac3be6e0
Harsh5840 <shuklaharsh5840@gmail.com>
2025-09-02 17:00:19 +0530

    kafka done

--- services/task-ingestor/src/kafkaProducer.js
+++ services/task-ingestor/src/kafkaProducer.js
@@ -1,7 +1,41 @@
-const {kafka} = require(kafkajs);
+const {Kafka} = require(kafkajs);
 
-const kafka = new kafka({
+const kafka = new Kafka({
     clientID: 'task-ingestor',
     brokers: [process.env.KAFKA_BROKER || 'localhost:9092',]
 })
 
+const producer = kafka.producer();
+
+async function connectProducer (){
+    try {
+        await producer.connect();
+        console.log("kafka producer connected");
+    } catch(err) {
+        console.error("failed to connect kafka producer");
+        process.exit(1);
+    }
+}
+
+async function sendTask(task) {
+    try{
+        await producer.send({
+            topic: process.env.KAFKA_TOPIC || 'tasks',
+            messages: [
+                {value: JSON.stringify(task)}
+            ],
+        });
+        console.log('Task sent to kafka :', task);
+    }
+    catch(err) {
+         console.error('❌ Error sending task to Kafka:', err);
+        throw err;
+    }
+}
+
+module.exports = {
+    kafkaProducer : {
+        connectProducer , 
+        sendTask
+    }
+}
